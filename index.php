d89775f7561569388a002dc66a52e2599ad70780
Yuri Emelyanov <y.emelyanov@bk.ru>
2026-03-24 00:21:50 +0300

    db connect, db query

--- index.php
+++ index.php
@@ -3,4 +3,30 @@
 
  $connection = new mySqli($host, $user, $pass, $data);
  if($connection->connect_error) die('connetction faelied');
+
+
+//запрос бд
+$query="SELECT * FROM monuments";
+$result=$connection->query($query);
+
+if(!$result) die ('query failed');
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
+
+
+$rows=$result->num_rows;
+for ($i=0; $i<$rows; ++$i){
+    $result->data_seek($i); 
+    $row=$result->fetch_assoc();
+
+    echo "type " . $row['monument_type'] . '<br>';
+    
+    
+    echo "data of row:" .print_r($row) . '<br>';
+}
+$result->close();
+$connection->close();
+
 ?>
